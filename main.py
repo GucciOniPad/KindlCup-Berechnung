330d8fab18a1efee483ca380bceba439f06ac4e5
Kian Shirazi <kian.shirazi@tum.de>
2025-05-22 15:11:17 +0200

    path handling update

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import os
 import csv_parser
 
 # TODO: Write Main which prompts user and does things (ask if we want to create final ranking or just rank discipline)
@@ -7,10 +8,10 @@ import csv_parser
 """
 def calculateDiscipline():
     print("Select discipline: 'b' - Biathlonstaffel, 'h1' - Hindernissprint U8/U10, 'h2' - Hindernissprint U12, 'k' - Stoßen, 'p' - Stabweitsprung")
-    discipline = input("Enter discipline: ")
-    # discipline = input("Enter discipline: 's' - Schlagwurf, 'd' - Drehwurf, 'f' - Fünfsprung, 'c' - Stadioncross\n")
-    input_file = input("Enter path to the input file:\n")
-    output_file = input("Enter path to the desired output file:\n")
+    discipline = input("Enter discipline: ").strip()
+    # discipline = input("Enter discipline: 's' - Schlagwurf, 'd' - Drehwurf, 'f' - Fünfsprung, 'c' - Stadioncross\n").strip()
+    input_file = os.path.normpath(input("Enter path to the input file:\n").strip())
+    output_file = os.path.normpath(input("Enter path to the desired output file:\n").strip())
 
     csv_parser.writeFileRankingCSV(input_file, discipline, "output_files/out_test.csv")
     csv_parser.writeTop6PerTeamCSV("output_files/out_test.csv", output_file, discipline)
